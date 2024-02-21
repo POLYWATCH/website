51396c49f26aad6d5784cba525769ffa5d997c5b
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-02-21 10:17:24 +0100

    third

--- pages/showroom.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import styles from '../styles/Home.module.css';
-import Spline from '@splinetool/react-spline';
-
-<Spline scene="https://prod.spline.design/0judBDVfq3t2BSFY/scene.splinecode" />
