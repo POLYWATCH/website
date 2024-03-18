a309568143f3a3f00da80989fb8677215c95bc16
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-03-18 16:32:55 +0100

    Update rewards.tsx

--- pages/rewards.tsx
+++ pages/rewards.tsx
@@ -88,18 +88,7 @@ const AboutUs: React.FC = () => {
   style={{ maxWidth: '100%', maxHeight: '100%' }}
   alt="Icona 3"
 /> 
-<p className={styles.heroSubtitle}>
-Customize it by choosing your color and telling us the social profile you want to link to the POLYWATCH T-shirt. 
 
-Anyone who scans the Qr Code on your T-shirt will be redirected to the profile of your choice
-      </p>
-<MediaRenderer
-  src="images/1e6.png"  
-  width="60%"
-  height="60%"
-  style={{ maxWidth: '100%', maxHeight: '100%' }}
-  alt="Icona 3"
-/> 
 <MediaRenderer
   src="images/1e5.png"  
   width="60%"
@@ -110,11 +99,21 @@ Anyone who scans the Qr Code on your T-shirt will be redirected to the profile o
 
 
       
-    
+<p className={styles.heroSubtitle}>
+Customize it by choosing your color and telling us the social profile you want to link to the POLYWATCH T-shirt. 
+
+Anyone who scans the Qr Code on your T-shirt will be redirected to the profile of your choice
+      </p>
+<MediaRenderer
+  src="images/789.png"  
+  width="60%"
+  height="60%"
+  style={{ maxWidth: '100%', maxHeight: '100%' }}
+  alt="Icona 3"
+/> 
         </div>
     
-  
-
+ 
 
 
     
