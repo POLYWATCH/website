794d88c6c7ef3a2311bba27a66f3afc6b2389034
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-04-07 14:30:56 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, phantomWallet, smartWallet, trustWallet } from "@thirdweb-dev/react";
-import Navbar from "../components/Navbar/Navbar";
 
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar/Navbar";
 
 
 const activeChain = "polygon";
@@ -13,22 +13,15 @@ const smartWalletConfig ={
   gasless: false
 
 }
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider 
     clientId="1f9299646db2cb55a1505c9032a58601"
     activeChain={activeChain}
     
-    supportedWallets={[
-      smartWallet(metamaskWallet(),smartWalletConfig),
-      smartWallet(coinbaseWallet(),smartWalletConfig),
-      smartWallet(trustWallet(),smartWalletConfig),
-      smartWallet(phantomWallet(),smartWalletConfig),
-      embeddedWallet()
-    
-    ]}
     >
-      
+    
       <Navbar/>
       <Component {...pageProps} />
     </ThirdwebProvider>
