b7baff0ea17928a8ca79ab1790898e5d70478259
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-04-17 16:07:04 +0200

    Update LineChart.tsx

--- components/LineChart.tsx
+++ components/LineChart.tsx
@@ -4,10 +4,10 @@ import { LineChart } from '@mui/x-charts/LineChart';
 export default function MarkOptimization() {
   return (
     <LineChart
-      xAxis={[{ data: [0.001, 0.02, 0.04, 0.06, 0.08, 0.1] }]}
+      xAxis={[{ data: [0.001, 0.02, 0.04, 0.06, 0.07, 0.08] }]}
       series={[
         {
-          data: [0.002, 0.03, 0.055, 0.085, 0.09, 0.1], // Aggiornati i dati del grafico
+          data: [0.002, 0.03, 0.055, 0.065, 0.08, 0.08], // Aggiornati i dati del grafico
           showMark: ({ index }) => index % 2 === 0,
         },
       ]}
