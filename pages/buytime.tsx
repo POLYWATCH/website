967842f6df320e642e0175c5e7dc5fef64bb89e8
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-04-18 14:25:50 +0200

    Update buytime.tsx

--- pages/buytime.tsx
+++ pages/buytime.tsx
@@ -289,7 +289,7 @@ const barColor = 'hsl(120, 100%, 75%)'; // Verde chiaro
   {claimedSupply.isError && <p>Error loading data.</p>}
   {claimedSupply.data && (
     <div>
-      <p style={{ color: 'lightgreen' }}>TOTAL TIME COLLECTED: {claimedSupply.data.displayValue} / 1350000</p>
+      <p style={{ color: 'lightgreen' }}>TOTAL TIME COLLECTED: {claimedSupply.data.displayValue} / 1.350.000</p>
       <div style={{ backgroundColor: 'lightgrey', height: '20px', borderRadius: '5px', marginTop: '10px', overflow: 'hidden', border: '1px solid white' }}>
         <div style={{ backgroundColor: '#00ff00', height: '100%', width: `${(parseFloat(claimedSupply.data.displayValue) / 1350000) * 100}%`, borderRadius: '5px', transition: 'width 0.5s ease-in-out, background-color 0.5s ease-in-out' }}></div>
       </div>
