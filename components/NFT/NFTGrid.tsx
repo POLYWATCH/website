b4aa6c10c87852f416d5a4cbaa7c6c41fd946dd9
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-05-01 11:01:04 +0200

    Update NFTGrid.tsx

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -1,6 +1,6 @@
-import type { NFT as NFTType } from "@thirdweb-dev/sdk";
+import * as React from 'react';
+import { NFT as NFTType } from "@thirdweb-dev/sdk";
 import Link from "next/link";
-import React from "react";
 import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
 import Skeleton from "../Skeleton/Skeleton";
 import NFT from "./NFT";
@@ -13,40 +13,48 @@ type Props = {
   emptyText?: string;
 };
 
-export default function NFTGrid({
+export default function NFTCarousel({
   isLoading,
   data,
   overrideOnclickBehavior,
   emptyText = "No NFTs found for this collection.",
 }: Props) {
   return (
-    <div className={styles.nftGridContainer}>
+    <div className={styles.nftCarouselContainer}>
       {isLoading ? (
-        [...Array(20)].map((_, index) => (
-          <div key={index} className={styles.nftContainer}>
-            <Skeleton key={index} width={"100%"} height="312px" />
-          </div>
-        ))
-      ) : data && data.length > 0 ? (
-        data.map((nft) =>
-          !overrideOnclickBehavior ? (
-            <Link
-              href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-            >
-              <NFT nft={nft} />
-            </Link>
-          ) : (
-            <div
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-              onClick={() => overrideOnclickBehavior(nft)}
-            >
-              <NFT nft={nft} />
+        <div className={styles.skeletonContainer}>
+          {[...Array(20)].map((_, index) => (
+            <div key={index} className={styles.nftContainer}>
+              <Skeleton key={index} width={"100%"} height="312px" />
             </div>
-          )
-        )
+          ))}
+        </div>
+      ) : data && data.length > 0 ? (
+        <div className={styles.nftCarousel}>
+          {data.map((nft, index) =>
+            !overrideOnclickBehavior ? (
+              <Link
+                href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
+                key={nft.metadata.id}
+                className={styles.nftContainer}
+              >
+                <div className={styles.nftItem}>
+                  <NFT nft={nft} />
+                </div>
+              </Link>
+            ) : (
+              <div
+                key={nft.metadata.id}
+                className={styles.nftContainer}
+                onClick={() => overrideOnclickBehavior(nft)}
+              >
+                <div className={styles.nftItem}>
+                  <NFT nft={nft} />
+                </div>
+              </div>
+            )
+          )}
+        </div>
       ) : (
         <p>{emptyText}</p>
       )}
