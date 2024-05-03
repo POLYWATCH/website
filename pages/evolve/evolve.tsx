bebd4036d5dabb86c70629d1846d77fbf863e36c
POLYWATCH <130767623+POLYWATCH@users.noreply.github.com>
2024-05-03 16:10:25 +0200

    Update evolve.tsx

--- pages/evolve/evolve.tsx
+++ pages/evolve/evolve.tsx
@@ -1,65 +1,71 @@
-import { ConnectWallet, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import styles from "../../styles/evolve.module.css";
 import { NextPage } from "next";
 import { CONTRACT_ADDRESS } from "../../const/contractAddresses";
 import { NFTCard } from "../../components/NFTCard";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const address = useAddress();
-
-  const { contract } = useContract(CONTRACT_ADDRESS);
-
-  const {
-    data: ownedNFTs,
-    isLoading: isOwnedNFTsLoading,
-  } = useOwnedNFTs(contract, address);
-
-  if(!address) {
-    return (
-      <div className={styles.main}>
-        <div className={styles.centeredContainer}>
-          <div className={styles.centeredCard}>
-            <h1>Evolving POLYWATCH</h1>
-            <ConnectWallet />
+    const address = useAddress();
+  
+    const { contract } = useContract(CONTRACT_ADDRESS);
+  
+    const {
+      data: ownedNFTs,
+      isLoading: isOwnedNFTsLoading,
+    } = useOwnedNFTs(contract, address);
+  
+    if(!address) {
+      return (
+        <div className={styles.main}>
+          <div className={styles.centeredContainer}>
+            <div className={styles.centeredCard}>
+              <h1>Evolving POLYWATCH</h1>
+              <ConnectWallet />
+            </div>
           </div>
         </div>
-      </div>
-    );
-  };
-  return (
-    <main className={styles.main}>
-      <div style={{ margin: '100px' }}>
-  {/* Content of the first section */}
-</div>
-
-{/* Empty space */}
-<div style={{ height: '100px' }} />
-
-      <div className={styles.container}>
-        <div  style={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", maxHeight:"48px", marginTop: "10px"}}>
-          <h1>MY POLYWATCH</h1>
-
-            
-         
+      );
+    };
+    return (
+        <main className={styles.main}>
+        <div style={{ margin: '100px' }}>
+          {/* Content of the first section */}
+        </div>
+      
+        {/* Empty space */}
+        <div style={{ height: '100px' }} />
+      
+        <div className={styles.container}>
+          <div style={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", maxHeight: "48px", marginTop: "10px" }}>
+            <h1>MY POLYWATCH</h1>
+          </div>
+          <div className={styles.nftGrid}> {/* Aggiungi questa classe per la griglia */}
+            {!isOwnedNFTsLoading && (
+              ownedNFTs && ownedNFTs.length > 0 ? (
+                ownedNFTs.map((nft) => (
+                  <div key={nft.metadata.id} className={styles.nftCard}>
+                    <p className={styles.nftCardName}>{nft.metadata.name}</p>
+                    <Link href={`/evolve/${nft.metadata.id}`} style={{ width: "100%" }}>
+                      <button className={styles.button}>Train</button>
+                    </Link>
+                    <NFTCard nft={nft} />
+                  </div>
+                ))
+              ) : (
+                <p>No Polywatch</p>
+              )
+            )}
+          </div>
           
         </div>
-        <div className={styles.grid} style={{ marginTop: "20px"}}>
-          {!isOwnedNFTsLoading && (
-            ownedNFTs && ownedNFTs.length > 0 ? (
-              ownedNFTs.map((nft) => (
-                <NFTCard
-                  key={nft.metadata.id}
-                  nft={nft}
-                />
-              ))
-            ) : (
-              <p>No Polywatch</p>
-            )
-          )}
-        </div>
-      </div>
-    </main>
-  );
+        <div style={{ height: '200px' }} />
+
+
+
+      </main>
+      
+
+    );
 };
 
 export default Home;
